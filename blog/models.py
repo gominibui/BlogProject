4f9d4aaabb7878a4da2eee3076c4106060e7de16
gominibui <164637350+gominibui@users.noreply.github.com>
2024-10-28 09:40:31 +0200

    add func to uploads files

--- blog/models.py
+++ blog/models.py
@@ -25,10 +25,10 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.TextField(max_length=200)
-    image_name = models.CharField(max_length=100)
+    image_field = models.ImageField(upload_to='posts', null=True, blank=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
-    content = models.TextField(max_length=500, validators=[MinLengthValidator(10)])
+    content = models.TextField( validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts', null=True)
     tags = models.ManyToManyField(Tag)
 
