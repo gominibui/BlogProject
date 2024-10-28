4248b49825c21df30013791e9314face86bf5ca4
gominibui <164637350+gominibui@users.noreply.github.com>
2024-10-28 10:00:57 +0200

    fix Tags

--- blog/views.py
+++ blog/views.py
@@ -32,4 +32,7 @@ class SinglePostView(DetailView):
     template_name = 'blog/post-details.html'
     model = Post
 
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
