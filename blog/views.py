31f65fd478470708b7949c55c60c60f2a0fa4858
gominibui <164637350+gominibui@users.noreply.github.com>
2024-10-28 11:02:41 +0200

    add comments on detail-page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render,get_object_or_404
 # Create your views here.
-from django.views.generic import ListView,DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
 from .models import Post
+from .forms import CommentForm
+from django.views import View
 
 
 
@@ -28,11 +32,31 @@ class ALlPostView(ListView):
         return data
 
 
-class SinglePostView(DetailView):
-    template_name = 'blog/post-details.html'
-    model = Post
+class SinglePostView(View):
+    def get(self, request,slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            'post':post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all(),}
+        return render(request, "blog/post-details.html", context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post_detail-page", args=[slug]))
+
+        context = {
+            'post': post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all(),}
+
+
+        return render(request, "blog/post-details.html", context)
